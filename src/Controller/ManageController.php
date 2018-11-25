b7c2393b356d602ea7befa1e35859c6c555c7e80
Franck <f_nouvion@orange.fr>
2018-11-25 19:55:25 +0100

    symfonyprojettest

--- src/Controller/ManageController.php
+++ src/Controller/ManageController.php
@@ -6,10 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 use Doctrine\Common\Persistence\ObjectManager;
 
 use App\Entity\User;
+use App\Entity\Country;
 use App\Repository\UserRepository;
 use App\Form\ArticleType;
 
@@ -21,43 +23,48 @@ class ManageController extends AbstractController
      */
     public function index(Request $request, ObjectManager $manager)
     {
-        if($request->request->count() > 0){
+        $user = new User();
+     /*   if($request->request->count() > 0){
             $user = new User();
             $user->setSurname($request->request->get('surname'))
                  ->setName($request->request->get('name'))
-                 ->setEmail($request->request->get('surname'))
+                 ->setBirthDate(\DateTime::createFromFormat('Y-m-d', $request->request->get('birthDate')))
+                 ->setEmail($request->request->get('email'));
+
+
                  ->setGender($request->request->get('surname'))
                  ->setCountry($request->request->get('surname'))
                  ->setProfession($request->request->get('surname'));
 
             $manager->persist($user);
             $manager->flush();
-        }
+        }*/
 
-       /* $form = $this->createFormBuilder($user)
+        $form = $this->createFormBuilder($user)
                      ->add('surname')
                      ->add('name')
                      ->add('birthDate')
                      ->add('email')
                      ->add('gender')
-                     ->add('country')
+                     ->add('country', EntityType::class, [
+                         'class' => Country::class,
+                         'choice_label' => 'nom_fr_fr'
+                     ])
                      ->add('profession')
-                     ->getForm();*/
+                     ->getForm();
 
-      //  $form = $this->createForm(UserType::class, $user);
-
-      /*  $form->handleRequest($request);
+        $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
             $manager->persist($user);
             $manager->flush();
 
             return $this->redirectToRoute('/');
-        }*/
+        }
 
         return $this->render('manage/index.html.twig', [
             'controller_name' => 'ManageController',
-         //   'formUser' -> $form->createView()
+            'formUser' => $form->createView()
         ]);
     }
 
